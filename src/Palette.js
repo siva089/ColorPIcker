ac80e3d5bb8e1c6a6da3807b26dea88a31b61db0
siva <sivasankar@troopr.io>
2020-01-04 17:07:13 +0530

    added styles to slider

--- src/Palette.js
+++ src/Palette.js
@@ -22,9 +22,13 @@ this.setState({level})
         const colorBoxes=colors[level].map(color=><ColorBox background={color.hex} name={color.name} />)
         return (
             <div className="Palette">
+                <div className="slider">
+                  
                 <Slider defaultValue={level} min={100} max={900} step={100}
                 onAfterChange={this.changeLevel}
                 />
+              
+                </div>
                 <div className="Palette-colors">
                     {colorBoxes}
                 </div>
