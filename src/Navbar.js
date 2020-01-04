be7f6e89ade38adcdb0b0f9d4cb240edf328a53f
siva <sivasankar@troopr.io>
2020-01-04 19:19:41 +0530

    added snackbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,25 +2,34 @@ import React, { Component } from 'react'
 import Slider from 'rc-slider';
 import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
+import Snackbar from '@material-ui/core/Snackbar';
+// import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
 import 'rc-slider/assets/index.css';
 import "./Navbar.css"
+import { IconButton } from '@material-ui/core';
  class Navbar extends Component {
      constructor(props) {
          super(props)
      
          this.state = {
-              format:"hex"
+              format:"hex",
+              open:false
          }
          this.handleChange=this.handleChange.bind(this)
+         this.handleClose=this.handleClose.bind(this)
      }
      
      handleChange(e){
-this.setState({format:e.target.value})
+this.setState({format:e.target.value,open:true})
 this.props.handleChange(e.target.value)
+     }
+     handleClose(e){
+         this.setState({open:false})
      }
     render() {
         const {level,changeLevel}=this.props
-        const {format}=this.state
+        const {format,open}=this.state
         return (
             <header className="Navbar">
                 <div className="logo">
@@ -49,7 +58,15 @@ this.props.handleChange(e.target.value)
                 </Select>
 
                   </div>
+<Snackbar anchorOrigin={{vertical:"bottom",horizontal:"left"}} 
+onClose={this.handleClose}
+open={open} autoHideDuration={3000} message={<span id="messafe-id">Format Changed to {format}</span>}
+ContentProps={{"aria-describedby":"message-id"}}
 
+action={[
+    <IconButton onClick={this.handleClose}><CloseIcon/></IconButton>
+]}
+/>
             </header>
         )
     }
