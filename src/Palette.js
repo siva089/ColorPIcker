27f7961c24535f8575265a730d3b83968f615f79
siva <sivasankar@troopr.io>
2020-01-04 17:25:39 +0530

    navabaradded

--- src/Palette.js
+++ src/Palette.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import ColorBox from "./ColorBox"
-import Slider, { Range } from 'rc-slider';
-import 'rc-slider/assets/index.css';
+import Navbar from "./Navbar"
 import "./Palette.css"
  class Palette extends Component {
      constructor(props) {
@@ -22,13 +21,7 @@ this.setState({level})
         const colorBoxes=colors[level].map(color=><ColorBox background={color.hex} name={color.name} />)
         return (
             <div className="Palette">
-                <div className="slider">
-                  
-                <Slider defaultValue={level} min={100} max={900} step={100}
-                onAfterChange={this.changeLevel}
-                />
-              
-                </div>
+              <Navbar level={this.state.level} changeLevel={this.changeLevel}/>
                 <div className="Palette-colors">
                     {colorBoxes}
                 </div>
