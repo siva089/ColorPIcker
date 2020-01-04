57c19d743d7a8368a1d78647f0030e79d4361ca2
siva <sivasankar@troopr.io>
2020-01-04 23:47:58 +0530

    added footers

--- src/Palette.js
+++ src/Palette.js
@@ -20,7 +20,7 @@ this.setState({level})
         this.setState({format:value})
      }
     render() {
-        const {colors}=this.props.palette
+        const {colors,paletteName,emoji}=this.props.palette
         const {level,format}=this.state
         const colorBoxes=colors[level].map(color=><ColorBox background={color[format]} name={color.name} />)
         return (
@@ -29,7 +29,10 @@ this.setState({level})
                 <div className="Palette-colors">
                     {colorBoxes}
                 </div>
-           
+           <footer className="Palette-footer">
+{paletteName}
+        <span className="emoji">{emoji}</span>
+           </footer>
             </div>
         )
     }
